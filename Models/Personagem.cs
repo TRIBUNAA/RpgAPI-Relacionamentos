91b81ca6512b999da784ab75b4b351d884edccf7
Vinicius <vitrigom5@gmail.com>
2024-04-17 20:39:15 -0300

    relacionamentos

---
+++ Models/Personagem.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using LV.Models;
+using LV.Models.Enuns;
+using RpgApi.Models.Enuns;
+
+namespace RpgApi.Models
+{
+    public class Personagem
+    {
+        //Atalho: PROP + TAB
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; } 
+        public ClasseEnum Classe { get; set; }
+        public byte[]? FotoPersonagem { get; set; } 
+        public int? UsuarioId { get; set; }
+        [JsonIgnore]
+        public Usuario? Usuario { get; set; } 
+
+         [JsonIgnore]
+         public Armas? Armas { get; set; } 
+         public int Disputas { get; set; } 
+         public int Vitorias { get; set; } 
+         public int Derrotas { get; set; } 
+         public List<PersonagemHabilidade> PersonagemHabilidades { get; set; } = [];
+
+        
+    }
+
+
+
+}
